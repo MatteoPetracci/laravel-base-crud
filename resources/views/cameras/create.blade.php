b81f88593d62af96f6f567bf606b37eb5a4501db
Matteo <matteop18@gmail.com>
2020-03-24 15:27:53 +0100

    prima modifica create

--- resources/views/cameras/create.blade.php
+++ resources/views/cameras/create.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@extends('layouts.layout')
+
 <body>
     <form action="{{route('cameras.store')}}" method="post">
         @csrf
@@ -18,3 +12,4 @@
     </form>
 </body>
 </html>
+
